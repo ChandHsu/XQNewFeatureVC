fbbbb76388d9a74fcaf63a93c3e457ed2137404b
qianggeProgramer <xuqiang.0205@qq.com>
2015-10-14 13:32:17 +0800

    展示新特性页面第一版

---
+++ XQNewFeatureVC/Controllers/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  XQNewFeatureVC
+//
+//  Created by 徐强 on 15/10/13.
+//  Copyright © 2015年 xuqiang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor colorWithRed:100 green:30 blue:160 alpha:1];
+
+}
+
+@end
---
+++ XQNewFeatureVC/Controllers/ViewController3.h
@@ -0,0 +1,13 @@
+//
+//  ViewController3.h
+//  XQNewFeatureVC
+//
+//  Created by 徐强 on 15/10/14.
+//  Copyright © 2015年 xuqiang. All rights reserved.
+//
+
+#import "XQNewFeatureBaseVc.h"
+
+@interface ViewController3 : XQNewFeatureBaseVc
+
+@end
